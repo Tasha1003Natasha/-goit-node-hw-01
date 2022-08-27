1defcf546d29b49bc015b70f7ca3dfeb9b3ff3b7
Tasha1003Natasha <0955712824n@gmail.com>
2022-08-27 13:28:21 +0300

    Готові 3 функції

--- db/contacts.js
+++ db/contacts.js
@@ -1,24 +1,72 @@
-import module from "path";
-import fs from "fs";
-
 const fs = require("fs").promises;
 const path = require("path");
 
-const contactsPath = fs.readFile(path.resolve("./contacts.json", "utf8"));
+const contactsPath = path.resolve(__dirname, "./contacts.json");
 
 // TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    let listContacts = JSON.parse(data);
+    console.table(listContacts);
+  } catch (error) {
+    console.error(`Got an error trying to read the file: ${error}`);
+  }
 }
 
-function getContactById(contactId) {
-  // ...твій код
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    let listContacts = JSON.parse(data);
+    const getContact = listContacts.find((contact) => contact.id === contactId);
+    console.table(getContact);
+  } catch (error) {
+    console.error(`Got an error trying to delete the file: ${error}`);
+  }
 }
 
-function removeContact(contactId) {
-  // ...твій код
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    let listContacts = JSON.parse(data);
+    const removeList = listContacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    let parseContact = JSON.stringify(removeList);
+    console.table(parseContact);
+    const removeContact = await fs.writeFile(
+      contactsPath,
+      parseContact,
+      "utf8"
+    );
+    console.table(removeContact);
+  } catch (error) {
+    console.error(`Got an error trying to delete the file: ${error}`);
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твій код
-}
+//////////////////////////////////////////////////////
+
+// async function addContact(name, email, phone) {
+//   try {
+//     let addContact = JSON.stringify(name, email, phone);
+//     // const addContact = `${name},${email},${phone}`;
+//     // await fs.appendFile(contactsPath, addContact);
+//     await fs.appendFile(contactsPath, {
+//       addContact: {
+//         name: `${name}`,
+//         email: `${email}`,
+//         phone: `${phone}`,
+//       },
+//     });
+//   } catch (error) {
+//     console.error(`Got an error trying to write to a file: ${error}`);
+//   }
+// }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log(123);
+// console.log(123);
+const contacts = require("./db/contacts");
+
+// contacts.listContacts("list");
+// contacts.getContactById("2");
+// contacts.removeContact("1");
+contacts.addContact("Nina", "nina@ukr.net", "(295) 841-6786");
